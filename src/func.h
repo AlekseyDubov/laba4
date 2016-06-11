402aded0894853e3bcd236f6239d931c8bb83e75
AlekseyDubov <freq731@yandex.ru>
2016-06-11 10:23:32 +0600

    Add program files

---
+++ src/func.h
@@ -0,0 +1,6 @@
+#ifndef FUNC_H
+#define FUNC_H
+
+double diskr(float a, float b, float c);
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "func.h"
+#include <math.h>
+
+int main()
+{
+	float a, b, c;
+	printf ("Enter a, b, c: ");
+	scanf ("%f %f %f", &a, &b, &c);
+
+	if (a == 0)
+	{
+		if (b == 0)
+		{
+			if (c == 0)
+			printf ("x - whatever");
+			else
+			printf ("no roots");
+		}
+		else
+		printf ("x = %g", - c / b);
+	}
+	else
+	diskr (a, b, c);
+	return 0;
+}
